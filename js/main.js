a7b629d40cfb52dd0ede1bf629b546c2f8c243ba
Oleksandr0007 <yagodinetc@gmail.com>
2022-12-23 18:10:11 +0200

    Update 2022-12-23T16:09:50.516Z

--- js/main.js
+++ js/main.js
@@ -1,93 +1,99 @@
 $(function(){
-// slider
-    $('.stage__slider').slick({
-        dots: false,
-        speed: 300,
-        variableWidth: true,
+// header main slider
+    $('.header__slider').slick({
         infinite: true,
+        fade: true,
+        prevArrow: '<img class="slider-arrows slider-arrows__left" src="img/arrows-left.svg" alt=""></img>',
+        nextArrow: '<img class="slider-arrows slider-arrows__right" src="img/arrows-right.svg" alt=""></img>',
+        asNavFor: '.slider-dotshead',
     });
-// burger
-    $(document).ready(function () {
-        $('#list > li').click(function (event) {
-            $(this).children("ul").slideToggle();
-            event.stopPropagation();
-        });
+    // header dots slider
+    $('.slider-dotshead').slick({
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        asNavFor: '.header__slider',
+    });
+    // end header dots slider
+// end header main slider
+// block SURF main slider
+    $('.serf-slider').slick({
+        slidesToShow: 4, 
+        slidesToScroll: 1,
+        prevArrow: '<img class="slider-arrows slider-arrows__left" src="img/arrows-left.svg" alt=""></img>',
+        nextArrow: '<img class="slider-arrows slider-arrows__right" src="img/arrows-right.svg" alt=""></img>',
+        asNavFor: '.slider-map'
+    });
+// end block SURF main slider
+// block SURF slider dots on map
+    $('.slider-map').slick({
+        slidesToShow: 8,
+        slidesToScroll: 1,
+        arrows: false,
+        asNavFor: '.serf-slider',
+        focusOnSelect: true
+    });
+// end block SURF slider dots on map
+// block TRAVEL main slider and block SHOP main slider
+    $('.holder__slider, .shop__slider').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        infinite: true,
+        fade: true,
+        prevArrow: '<img class="slider-arrows slider-arrows__left" src="img/arrows-left.svg" alt=""></img>',
+        nextArrow: '<img class="slider-arrows slider-arrows__right" src="img/arrows-right.svg" alt=""></img>',
+    });
+// end block TRAVEL main slider and block SHOP main slider
+// calc sum og night and guests script
+    $('.quantity-button').on('click', function(){
+        let summ = $('.nights').val() * $('.summ').data('nights') + ($('.guests').val() - 1) * $('.summ').data('guests');
+        $('.summ').html('$' + summ)
     });
 
-    $(document).ready(function(){
-        $('#item1').click(function () {
-            $('.item__box1').toggleClass('underline');
-            $('.item__box-img1').toggleClass('underline1');
-            });
-        });
-    $(document).ready(function(){
-        $('#item2').click(function () {
-            $('.item__box22').toggleClass('underline');
-            $('.item__box-img2').toggleClass('underline1');
-            });
-        });
-    $(document).ready(function(){
-        $('#item3').click(function () {
-            $('.item__box3').toggleClass('underline');
-            $('.item__box-img3').toggleClass('underline1');
-            });
-        });
-    $(document).ready(function(){
-        $('#item4').click(function () {
-            $('.item__box4').toggleClass('underline');
-            $('.item__box-img4').toggleClass('underline1');
-            });
-        });
-    $(document).ready(function(){
-        $('#item5').click(function () {
-            $('.item__box5').toggleClass('underline');
-            $('.item__box-img5').toggleClass('underline1');
-            });
-        });
-});
-// header on mobile
-const burgerMenu = document.getElementById("burger");
-const navbarMenu = document.getElementById("menu");
-
-// Show and Hide Navbar Menu
-burgerMenu.addEventListener("click", () => {
-	burgerMenu.classList.toggle("is-active");
-	navbarMenu.classList.toggle("is-active");
-
-	if (navbarMenu.classList.contains("is-active")) {
-		navbarMenu.style.maxHeight = navbarMenu.scrollHeight + "px";
-	} else {
-		navbarMenu.removeAttribute("style");
-	}
+    let summ = $('.nights').val() * $('.summ').data('nights') + ($('.guests').val() - 1) * $('.summ').data('guests');
+    $('.summ').html('$' + summ)
+// end calc sum og night and guests script
+// block SHOP animation clock on dots at board
+    $('.surfboard-box__circle').on('click', function () {
+        $(this).toggleClass('active')
+    });
+// end block SHOP animation clock on dots at board
+// menu adaptive animation
+    $('.menu-btn').on('click', function () {
+        $('.menu').toggleClass('menu-active')
+    });
+// end menu adaptive animation
 });
 
-$(function() {
-    $(".buttonSend").on("click", validate);
-   
-    // Validate email
-    function validateEmail(email) {
-      var re = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
-      return re.test(String(email).toLowerCase());
-    }
-     
-    // send form
-    function sendForm() {
-      $(".error").text("Отправлено!").fadeIn();
-    }
-   
-    // validate email and send form after success validation
-    function validate() {
-      var email = $(".form__input2").val();
-      var $error = $(".error");
-      $error.text("");
-   
-      if (validateEmail(email)) {
-        $error.fadeOut();
-        sendForm();
-      } else {
-        $error.fadeIn();
-        $error.text("Email введен не правильно");
-      }
-      return false;
-    }
-  });
+// input type number script
+$('<div class="quantity-nav"><div class="quantity-button quantity-up"><img src="img/plus.svg" alt=""></div><div class="quantity-button quantity-down"><img src="img/minus.svg" alt=""></div></div>').insertAfter('.quantity input');
+$('.quantity').each(function () {
+    var spinner = $(this),
+        input = spinner.find('input[type="number"]'),
+        btnUp = spinner.find('.quantity-up'),
+        btnDown = spinner.find('.quantity-down'),
+        min = input.attr('min'),
+        max = input.attr('max');
+
+    btnUp.click(function () {
+        var oldValue = parseFloat(input.val());
+        if (oldValue >= max) {
+            var newVal = oldValue;
+        } else {
+            var newVal = oldValue + 1;
+        }
+        spinner.find("input").val(newVal);
+        spinner.find("input").trigger("change");
+    });
+
+    btnDown.click(function () {
+        var oldValue = parseFloat(input.val());
+        if (oldValue <= min) {
+            var newVal = oldValue;
+        } else {
+            var newVal = oldValue - 1;
+        }
+        spinner.find("input").val(newVal);
+        spinner.find("input").trigger("change");
+    });
+});
+// end input type number script
